10fd2c5f9d20ac89b2d05d95d925824e9e9c6616
Tra1er <93647880+Tra1er@users.noreply.github.com>
2024-11-14 20:16:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -15,81 +15,10 @@ const animeList = [
           { name: "vidhidepre", url: "https://vidhidepre.com/embed/gddd7a6iilqh" },
           { name: "listeamed", url: "https://listeamed.net/e/k3gG5q3lnRjE1N2" }
         ]
-      },
-      {
-        episode: 3,
-        players: [
-          { name: "vidhidepre", url: "https://vidhidepre.com/embed/c6lqbi6jwjrf" },
-          { name: "listeamed", url: "https://listeamed.net/e/mMq75L80W8V5XPB" }
-        ]
-      },
-      {
-        episode: 4,
-        players: [
-          { name: "vidhidepre", url: "https://vidhidepre.com/embed/76ah15iaaknq" },
-          { name: "listeamed", url: "https://listeamed.net/e/qA3WOmGX7j1xKkz" }
-        ]
-      },
-      {
-        episode: 5,
-        players: [
-          { name: "vidhidepre", url: "https://vidhidepre.com/embed/11dp6pzg6wtm" },
-          { name: "listeamed", url: "https://listeamed.net/e/qM1VOPN6ZV8xo4k" }
-        ]
-      },
-      {
-        episode: 6,
-        players: [
-          { name: "vidhidepre", url: "https://vidhidepre.com/embed/eb12bkotyb9t" },
-          { name: "listeamed", url: "https://listeamed.net/e/3Q0lxBWp68MEj1J" }
-        ]
-      },
-      {
-        episode: 7,
-        players: [
-          { name: "vidhidepre", url: "https://vidhidepre.com/embed/a7wvqk00fw1e" },
-          { name: "listeamed", url: "https://listeamed.net/e/nBLvbZtEIVq1B6V" }
-        ]
-      },
-      {
-        episode: 8,
-        players: [
-          { name: "vidhidepre", url: "https://vidhidepre.com/embed/8fyz0wh8zjx9" },
-          { name: "listeamed", url: "https://listeamed.net/e/gs68F8DN2Q2jeDo" }
-        ]
-      },
-      {
-        episode: 9,
-        players: [
-          { name: "vidhidepre", url: "https://vidhidepre.com/embed/zrkk6hdjywft" },
-          { name: "listeamed", url: "https://listeamed.net/e/zvXtnaWckrh3s1G" }
-        ]
-      },
-      {
-        episode: 10,
-        players: [
-          { name: "vidhidepre", url: "https://vidhidepre.com/embed/xgeq0pm9q4fr" },
-          { name: "listeamed", url: "https://listeamed.net/e/0RbhXsdtm3v39kT" }
-        ]
-      },
-      {
-        episode: 11,
-        players: [
-          { name: "vidhidepre", url: "https://vidhidepre.com/embed/xykzmvzx5jpx" },
-          { name: "listeamed", url: "https://listeamed.net/e/ZqUp3wsdfr1jC0k" }
-        ]
-      },
-      {
-        episode: 12,
-        players: [
-          { name: "vidhidepre", url: "https://vidhidepre.com/embed/3h5vddt7e9vj" },
-          { name: "listeamed", url: "https://listeamed.net/e/Qmi7gnMXG9oFAon" }
-        ]
       }
     ]
   }
 ];
-
 
 // Funkcja do generowania opcji dla wyboru anime
 function populateAnimeSelect() {
