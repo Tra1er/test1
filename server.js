a2c5154b378a7ab7926b1a6b65f3f0fda2f03bd2
Tra1er <93647880+Tra1er@users.noreply.github.com>
2024-11-14 20:06:25 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,165 +1,22 @@
-{
-  "anime": [
-    {
-      "title": "Spy x Family",
-      "episodes": [
-        {
-          "episode": 1,
-          "players": [
-            {
-              "name": "vidhidepre",
-              "url": "https://vidhidepre.com/embed-en6ij3y36jeh.html"
-            },
-            {
-              "name": "listeamed",
-              "url": "https://listeamed.net/embed/en6ij3y36jeh"
-            }
-          ]
-        },
-        {
-          "episode": 2,
-          "players": [
-            {
-              "name": "vidhidepre",
-              "url": "https://vidhidepre.com/embed-gddd7a6iilqh.html"
-            },
-            {
-              "name": "listeamed",
-              "url": "https://listeamed.net/embed/gddd7a6iilqh"
-            }
-          ]
-        },
-        {
-          "episode": 3,
-          "players": [
-            {
-              "name": "vidhidepre",
-              "url": "https://vidhidepre.com/embed-c6lqbi6jwjrf.html"
-            },
-            {
-              "name": "listeamed",
-              "url": "https://listeamed.net/embed/c6lqbi6jwjrf"
-            }
-          ]
-        },
-        {
-          "episode": 4,
-          "players": [
-            {
-              "name": "vidhidepre",
-              "url": "https://vidhidepre.com/embed-76ah15iaaknq.html"
-            },
-            {
-              "name": "listeamed",
-              "url": "https://listeamed.net/embed/76ah15iaaknq"
-            }
-          ]
-        },
-        {
-          "episode": 5,
-          "players": [
-            {
-              "name": "vidhidepre",
-              "url": "https://vidhidepre.com/embed-11dp6pzg6wtm.html"
-            },
-            {
-              "name": "listeamed",
-              "url": "https://listeamed.net/embed/11dp6pzg6wtm"
-            }
-          ]
-        },
-        {
-          "episode": 6,
-          "players": [
-            {
-              "name": "vidhidepre",
-              "url": "https://vidhidepre.com/embed-eb12bkotyb9t.html"
-            },
-            {
-              "name": "listeamed",
-              "url": "https://listeamed.net/embed/eb12bkotyb9t"
-            }
-          ]
-        },
-        {
-          "episode": 7,
-          "players": [
-            {
-              "name": "vidhidepre",
-              "url": "https://vidhidepre.com/embed-c65tdcwfci8g.html"
-            },
-            {
-              "name": "listeamed",
-              "url": "https://listeamed.net/embed/c65tdcwfci8g"
-            }
-          ]
-        },
-        {
-          "episode": 8,
-          "players": [
-            {
-              "name": "vidhidepre",
-              "url": "https://vidhidepre.com/embed-92oq10et63bt.html"
-            },
-            {
-              "name": "listeamed",
-              "url": "https://listeamed.net/embed/92oq10et63bt"
-            }
-          ]
-        },
-        {
-          "episode": 9,
-          "players": [
-            {
-              "name": "vidhidepre",
-              "url": "https://vidhidepre.com/embed-q6i53dm68qng.html"
-            },
-            {
-              "name": "listeamed",
-              "url": "https://listeamed.net/embed/q6i53dm68qng"
-            }
-          ]
-        },
-        {
-          "episode": 10,
-          "players": [
-            {
-              "name": "vidhidepre",
-              "url": "https://vidhidepre.com/embed-5ac98jymjo7a.html"
-            },
-            {
-              "name": "listeamed",
-              "url": "https://listeamed.net/embed/5ac98jymjo7a"
-            }
-          ]
-        },
-        {
-          "episode": 11,
-          "players": [
-            {
-              "name": "vidhidepre",
-              "url": "https://vidhidepre.com/embed-p14hy5jtijmj.html"
-            },
-            {
-              "name": "listeamed",
-              "url": "https://listeamed.net/embed/p14hy5jtijmj"
-            }
-          ]
-        },
-        {
-          "episode": 12,
-          "players": [
-            {
-              "name": "vidhidepre",
-              "url": "https://vidhidepre.com/embed-rh9o43djnkfh.html"
-            },
-            {
-              "name": "listeamed",
-              "url": "https://listeamed.net/embed/rh9o43djnkfh"
-            }
-          ]
-        }
-      ]
-    }
-  ]
-}
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+// Serwowanie plików statycznych (CSS, JS, itp.)
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Endpoint do pobierania animeData.json
+app.get('/animeData.json', (req, res) => {
+    res.sendFile(path.join(__dirname, 'animeData.json'));
+});
+
+// Strona główna
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Serwer działa na http://localhost:${port}`);
+});
