ff334b641a0daaf770ccb075102de19d1209d20a
Tra1er <93647880+Tra1er@users.noreply.github.com>
2024-11-14 21:20:01 +0100

    Create animeData.js

---
+++ animeData.js
@@ -0,0 +1,23 @@
+// animeData.js
+const animeData = {
+    anime: [
+        {
+            title: "Spy x Family",
+            description: "Historia rodziny szpiegów w świecie pełnym tajemnic i akcji.",
+            episodes: [
+                { episodeNumber: 1, vidHide: "https://vidhide.com/episode1", vidGuard: "https://vidguard.com/episode1" },
+                { episodeNumber: 2, vidHide: "https://vidhide.com/episode2", vidGuard: "https://vidguard.com/episode2" },
+                { episodeNumber: 3, vidHide: "https://vidhide.com/episode3", vidGuard: "https://vidguard.com/episode3" },
+                { episodeNumber: 4, vidHide: "https://vidhide.com/episode4", vidGuard: "https://vidguard.com/episode4" },
+                { episodeNumber: 5, vidHide: "https://vidhide.com/episode5", vidGuard: "https://vidguard.com/episode5" },
+                { episodeNumber: 6, vidHide: "https://vidhide.com/episode6", vidGuard: "https://vidguard.com/episode6" },
+                { episodeNumber: 7, vidHide: "https://vidhide.com/episode7", vidGuard: "https://vidguard.com/episode7" },
+                { episodeNumber: 8, vidHide: "https://vidhide.com/episode8", vidGuard: "https://vidguard.com/episode8" },
+                { episodeNumber: 9, vidHide: "https://vidhide.com/episode9", vidGuard: "https://vidguard.com/episode9" },
+                { episodeNumber: 10, vidHide: "https://vidhide.com/episode10", vidGuard: "https://vidguard.com/episode10" },
+                { episodeNumber: 11, vidHide: "https://vidhide.com/episode11", vidGuard: "https://vidguard.com/episode11" },
+                { episodeNumber: 12, vidHide: "https://vidhide.com/episode12", vidGuard: "https://vidguard.com/episode12" }
+            ]
+        }
+    ]
+};
