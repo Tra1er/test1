df3ddd4b341a1f4204fc292f9acb3eee0611dc78
Tra1er <93647880+Tra1er@users.noreply.github.com>
2024-11-14 21:24:43 +0100

    Update animeData.js

--- animeData.js
+++ animeData.js
@@ -5,18 +5,18 @@ const animeData = {
             title: "Spy x Family",
             description: "Historia rodziny szpiegów w świecie pełnym tajemnic i akcji.",
             episodes: [
-                { episodeNumber: 1, vidHide: "https://vidhide.com/episode1", vidGuard: "https://vidguard.com/episode1" },
-                { episodeNumber: 2, vidHide: "https://vidhide.com/episode2", vidGuard: "https://vidguard.com/episode2" },
-                { episodeNumber: 3, vidHide: "https://vidhide.com/episode3", vidGuard: "https://vidguard.com/episode3" },
-                { episodeNumber: 4, vidHide: "https://vidhide.com/episode4", vidGuard: "https://vidguard.com/episode4" },
-                { episodeNumber: 5, vidHide: "https://vidhide.com/episode5", vidGuard: "https://vidguard.com/episode5" },
-                { episodeNumber: 6, vidHide: "https://vidhide.com/episode6", vidGuard: "https://vidguard.com/episode6" },
-                { episodeNumber: 7, vidHide: "https://vidhide.com/episode7", vidGuard: "https://vidguard.com/episode7" },
-                { episodeNumber: 8, vidHide: "https://vidhide.com/episode8", vidGuard: "https://vidguard.com/episode8" },
-                { episodeNumber: 9, vidHide: "https://vidhide.com/episode9", vidGuard: "https://vidguard.com/episode9" },
-                { episodeNumber: 10, vidHide: "https://vidhide.com/episode10", vidGuard: "https://vidguard.com/episode10" },
-                { episodeNumber: 11, vidHide: "https://vidhide.com/episode11", vidGuard: "https://vidguard.com/episode11" },
-                { episodeNumber: 12, vidHide: "https://vidhide.com/episode12", vidGuard: "https://vidguard.com/episode12" }
+                { episodeNumber: 1, vidHide: "https://vidhidepre.com/embed/en6ij3y36jeh", vidGuard: "https://listeamed.net/e/8ozgENLQAN65mjA" },
+                { episodeNumber: 2, vidHide: "https://vidhidepre.com/embed/gddd7a6iilqh", vidGuard: "https://listeamed.net/e/k3gG5q3lnRjE1N2" },
+                { episodeNumber: 3, vidHide: "https://vidhidepre.com/embed/c6lqbi6jwjrf", vidGuard: "https://listeamed.net/e/mMq75L80W8V5XPB" },
+                { episodeNumber: 4, vidHide: "https://vidhidepre.com/embed/76ah15iaaknq", vidGuard: "https://listeamed.net/e/qA3WOmGX7j1xKkz" },
+                { episodeNumber: 5, vidHide: "https://vidhidepre.com/embed/11dp6pzg6wtm", vidGuard: "https://listeamed.net/e/qM1VOPN6ZV8xo4k" },
+                { episodeNumber: 6, vidHide: "https://vidhidepre.com/embed/eb12bkotyb9t", vidGuard: "https://listeamed.net/e/3Q0lxBWp68MEj1J" },
+                { episodeNumber: 7, vidHide: "https://vidhidepre.com/embed/a7wvqk00fw1e", vidGuard: "https://listeamed.net/e/nBLvbZtEIVq1B6V" },
+                { episodeNumber: 8, vidHide: "https://vidhidepre.com/embed/8fyz0wh8zjx9", vidGuard: "https://listeamed.net/e/gs68F8DN2Q2jeDo" },
+                { episodeNumber: 9, vidHide: "https://vidhidepre.com/embed/zrkk6hdjywft", vidGuard: "https://listeamed.net/e/zvXtnaWckrh3s1G" },
+                { episodeNumber: 10, vidHide: "https://vidhidepre.com/embed/xgeq0pm9q4fr", vidGuard: "https://listeamed.net/e/0RbhXsdtm3v39kT" },
+                { episodeNumber: 11, vidHide: "https://vidhidepre.com/embed/xykzmvzx5jpx", vidGuard: "https://listeamed.net/e/ZqUp3wsdfr1jC0k" },
+                { episodeNumber: 12, vidHide: "https://vidhidepre.com/embed/3h5vddt7e9vj", vidGuard: "https://listeamed.net/e/Qmi7gnMXG9oFAon" }
             ]
         }
     ]
